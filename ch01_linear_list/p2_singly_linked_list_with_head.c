bf93f98a45ab42b8bebbad7284ec8406eed91eac
JacksonZHR <1024060689@qq.com>
2024-04-20 16:39:09 +0800

    feat: 增加使用单链表实现有序表的合并

--- ch01_linear_list/p2_singly_linked_list_with_head.c
+++ ch01_linear_list/p2_singly_linked_list_with_head.c
@@ -13,7 +13,7 @@ typedef int Status;
 #define false 0;
 
 typedef int ElemType;
-typedef struct LNode{
+typedef struct LNode {
     ElemType data;
     struct LNode *next;
 } LNode, *LinkedList;
@@ -24,11 +24,11 @@ typedef struct LNode{
  */
 void PrintInfo(LinkedList L) {
     //1. 定位首元位置（因为是带头结点的）
-    LNode * cur_point = L ->next;
+    LNode *cur_point = L->next;
     printf("\n______\n");
     printf("ele\n");
     while (cur_point != NULL) {
-        printf("%d",cur_point->data);
+        printf("%d", cur_point->data);
         if (cur_point->next == NULL) break;
         printf("->");
         cur_point = cur_point->next;
@@ -42,20 +42,21 @@ void PrintInfo(LinkedList L) {
  * @param L
  * @return
  */
-Status InitList(LinkedList* L) {
+Status InitList(LinkedList *L) {
     *L = (LinkedList) malloc(sizeof(LNode));
     (*L)->data = 0;
     (*L)->next = NULL;
     return OK;
 }
+
 /**
  * 销毁线性表释放空间
  * @param L
  */
 void DestroyList(LinkedList *L) {
-    LNode* cur_point = *L;
-    LNode* next_point = NULL;
-    while(cur_point != NULL) {
+    LNode *cur_point = *L;
+    LNode *next_point = NULL;
+    while (cur_point != NULL) {
         next_point = cur_point->next;
         free(cur_point);
         cur_point = next_point;
@@ -68,9 +69,9 @@ void DestroyList(LinkedList *L) {
  * @param L
  */
 void ClearList(LinkedList *L) {
-    LNode* cur_point = (*L)->next;
-    LNode* next_point = NULL;
-    while(cur_point != NULL) {
+    LNode *cur_point = (*L)->next;
+    LNode *next_point = NULL;
+    while (cur_point != NULL) {
         next_point = cur_point->next;
         free(cur_point);
         cur_point = next_point;
@@ -110,7 +111,7 @@ int ListLength(LinkedList L) {
  * @param e
  * @return
  */
-Status GetElem(LinkedList L, int i, ElemType * e) {
+Status GetElem(LinkedList L, int i, ElemType *e) {
 //    if (i<1 || i> ListLength(L)) return Err;
 //    for (int j = 0; j < i; ++j) {
 //        L = L->next;
@@ -123,7 +124,7 @@ Status GetElem(LinkedList L, int i, ElemType * e) {
         L = L->next;
         j++;
     }
-    if(!L || j>i) return Err;
+    if (!L || j > i) return Err;
     *e = L->data;
     return OK;
 }
@@ -139,7 +140,7 @@ int LocateElemPosition(LinkedList L, ElemType e) {
     L = L->next;
     int j = 1;
     while (L && L->data != e) {
-        L= L->next;
+        L = L->next;
         j++;
     }
     if (L) return j;
@@ -155,15 +156,15 @@ int LocateElemPosition(LinkedList L, ElemType e) {
  */
 Status ListInsert(LinkedList L, ElemType e, int i) {
     int j = 0;
-    while(L && j < i - 1) { // 允许遍历到最后一个元素
-        L = L ->next;
+    while (L && j < i - 1) { // 允许遍历到最后一个元素
+        L = L->next;
         j++;
     }
-    if (!L || j > i -1) return Err;
+    if (!L || j > i - 1) return Err;
 
-    LNode* newNode = (LNode*) malloc(sizeof (LNode));
+    LNode *newNode = (LNode *) malloc(sizeof(LNode));
     newNode->data = e;
-    newNode->next  = L->next;
+    newNode->next = L->next;
     L->next = newNode;
     return OK;
 }
@@ -177,12 +178,12 @@ Status ListInsert(LinkedList L, ElemType e, int i) {
  */
 Status ListAdd(LinkedList L, ElemType e) {
     //1. 创建末尾的新结点
-    LNode* newNode = (LNode*) malloc(sizeof (LNode));
+    LNode *newNode = (LNode *) malloc(sizeof(LNode));
     newNode->data = e;
-    newNode->next  = NULL;
+    newNode->next = NULL;
 
     //2. 找到当前的末尾结点
-    LNode* cur_point = L;
+    LNode *cur_point = L;
     while (cur_point->next != NULL) {
         cur_point = cur_point->next;
     }
@@ -200,14 +201,14 @@ Status ListAdd(LinkedList L, ElemType e) {
  * @param i
  * @return
  */
-Status ListDelete(LinkedList L, ElemType* e, int i) {
+Status ListDelete(LinkedList L, ElemType *e, int i) {
     int j = 0;
-    while(L -> next && j < i - 1) { // 不允许遍历到最后一个元素
+    while (L->next && j < i - 1) { // 不允许遍历到最后一个元素
         L = L->next;
         j++;
     }
-    if(!(L->next) || j > i-1) return Err; // 不能是最后一个元素，不能是第0个
-    LNode * free_node = L->next;
+    if (!(L->next) || j > i - 1) return Err; // 不能是最后一个元素，不能是第0个
+    LNode *free_node = L->next;
     L->next = free_node->next;
     *e = free_node->data;
 
@@ -217,20 +218,57 @@ Status ListDelete(LinkedList L, ElemType* e, int i) {
 
 }
 
+/**
+ * 使用链表将有序表合并到一个表当中去
+ * @param LA
+ * @param LB
+ */
+void UnionList(LinkedList LA, LinkedList LB) {
+    LNode *newListPoint;
+    newListPoint = LA;
+    LNode *iterator_a = LA->next;
+    LNode *iterator_b = LB->next;
+
+    while (iterator_a != NULL && iterator_b != NULL) {
+        if (iterator_a->data <= iterator_b->data) {
+            newListPoint->next = iterator_a;
+            newListPoint = iterator_a;
+            iterator_a = iterator_a -> next;
+
+        } else {
+            newListPoint->next = iterator_b;
+            newListPoint = iterator_b;
+            iterator_b = iterator_b->next;
+        }
+    }
+    if (iterator_a != NULL) {
+        newListPoint->next = iterator_a;
+    }
+    if (iterator_b != NULL) {
+        newListPoint->next = iterator_b;
+    }
+
+}
 
 
 int main() {
-    LinkedList L = NULL;
-    InitList(&L);
-    ListAdd(L,1);
-    ListAdd(L,2);
-    ListAdd(L,3);
-    ListAdd(L,4);
-    ListAdd(L,6);
-    PrintInfo(L);
-    int a = 0;
-    ListDelete(L, &a, 5);
-    PrintInfo(L);
+    LinkedList LA = NULL;
+    LinkedList LB = NULL;
+    InitList(&LA);
+    InitList(&LB);
+
+    for (int i = 0; i < 10; i = i+2) {
+        ListAdd(LA,i);
+    }
+    for (int i = 1; i < 18; i = i+2) {
+        ListAdd(LB,i);
+    }
+
+    PrintInfo(LA);
+    PrintInfo(LB);
+
+    UnionList(LA,LB);
+    PrintInfo(LA);
 
 
 }
