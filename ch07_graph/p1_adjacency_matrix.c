516423e5b0f0b5cc44c0aca62ae5afee3fe771e6
JacksonZHR <1024060689@qq.com>
2024-05-03 22:48:51 +0800

    feat: 迪杰斯特拉算法求最短路径

--- ch07_graph/p1_adjacency_matrix.c
+++ ch07_graph/p1_adjacency_matrix.c
@@ -12,7 +12,7 @@ typedef int Bool;
 #define true 1
 #define false 0
 
-#define INFINITY INT_MAX
+#define INFINITY (INT_MAX/2) // 最大值不便使用加法
 #define MAX_VERTEX_NUM 20
 typedef int VRType;
 typedef char VertexType;
@@ -166,11 +166,105 @@ void DFSTraverse(MGraph G, int start) {
     printf("\n");
 }
 
+void ShortestPath_DIJ(MGraph G, char start) {
+    int start_index = LocateVex(G,start);
+    // 到各个点的最短路径的长度
+    int *D = (int *) malloc(G.vex_num*sizeof (int));
+    for (int i = 0; i < G.vex_num; ++i) {
+        D[i] = INFINITY; // 到自己也记为最远，方便在min的时候方便讨论
+    }
+
+    // 从start点到各个顶点路径上的前驱节点，例如 A到C的最短是A->B->D-C，那么indexC=indexD,indexD=indexB,indexB=indexA,indexA=indexA
+    int *Path = (int *) malloc(G.vex_num*sizeof (int ));
+    for (int i = 0; i < G.vex_num; ++i) {
+        Path[i] = -1; // -1表示不存在
+    }
+
+    // 一开始为start能够直接到达的点，故最初由路径长度和前驱结点
+    for (int i = 0; i < G.vex_num; ++i) {
+        if (G.arcs[start_index][i].data != INFINITY) {
+            D[i] = G.arcs[start_index][i].data;
+            Path[i] = start_index;
+        }
+    }
+
+    //记录各点情况
+    int * final = (int *) malloc(G.vex_num*sizeof (int));
+    for (int i = 0; i < G.vex_num; ++i) {
+        final[i] = 0;
+    }
+    final[start_index] = 1;
+//    //数组检查
+//    for (int i = 0; i < G.vex_num; ++i) {
+//        printf("%d.%d.%d\n", D[i],Path[i],final[i]);
+//    }
+
+    //求解
+    for (int i = 0; i < G.vex_num-1; ++i) { // 循环只需要结点数-1次
+    // 求得当前D中最短的路径
+        int min = INFINITY;
+        int min_index = -1;
+        for (int j = 0; j < G.vex_num; ++j) {
+            if (!final[j])
+                if(D[j] <= min) {
+                    min = D[j];
+                    min_index = j;
+                }
+        }
+        final[min_index] = 1; // 标记为已“遍历”
+
+        // 更新路径
+
+        for (int j = 0; j < G.vex_num; ++j) {
+            if(!final[j] && min +  G.arcs[min_index][j].data < D[j]) {
+                D[j] =  min +  G.arcs[min_index][j].data;
+                Path[j] = min_index;
+            }
+        }
+
+    }
+
+
+    //数组检查
+//    printf("----------------------\n");
+//    for (int i = 0; i < G.vex_num; ++i) {
+//        printf("%d.%d.%d\n", D[i],Path[i],final[i]);
+//    }
+
+    //打印路径情况
+    for (int i = 0; i < G.vex_num; ++i) {
+        if (i == start_index) {
+            printf("%c:self\n",G.vex[i]);
+            continue;
+        }
+        if (Path[i] == -1) {
+            printf("%c:no road\n",G.vex[i]);
+            continue;
+        }
+        int stack[G.vex_num];
+        int j=0;
+        int point = i;
+        while (Path[point]!=0) {
+            stack[j++] = Path[point];
+            point = Path[point];
+        }
+        stack[j++] = 0;
+        printf("%c:",G.vex[i]);
+        while (j!=0) {
+            printf("%c->",G.vex[stack[--j]]);
+        }
+        printf("%c (%d)\n",G.vex[i],D[i]);
+
+
+    }
+}
+
 
 int main() {
     MGraph G;
     CreateExampleGraphDN(&G);
     PrintGraph(G);
+    ShortestPath_DIJ(G,'A');
 
 };
 
