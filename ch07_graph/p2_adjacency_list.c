c71060059af471d7c093305a373d331144e1a331
JacksonZHR <1024060689@qq.com>
2024-05-03 11:16:55 +0800

    feat: 增加一个例子无向图

--- ch07_graph/p2_adjacency_list.c
+++ ch07_graph/p2_adjacency_list.c
@@ -17,6 +17,12 @@ typedef char VertexType;
 typedef enum {DG,DN,UDG,UDN} GraphKind; //有向图、有向网、无向图、无向网
 typedef int InfoType; // 弧信息
 
+struct edge {
+    char a;
+    char b;
+    int weight;
+};
+
 // 弧结点
 typedef struct ArcNode {
     int adj_vex; // 弧指向的顶点的位置
@@ -99,6 +105,61 @@ Status CreateUDN(ALGraph *G) {
     return OK;
 }
 
+// 构造例子无向网
+Status CreateExampleUDN(ALGraph *G) {
+    //1. 输入顶点数和边数
+    G->vex_num = 6;
+    G->arc_num = 10;
+    //2. 存储顶点信息
+    char chars[] = {'A','B','C','D','E','F'};
+    for (int i = 0; i < G->vex_num; ++i) {
+        G->vertices[i].data = chars[i];
+    }
+    //3. 初始化邻接矩阵
+    G->graphKind = UDN;
+    for (int i = 0; i < G->vex_num; ++i) {
+        G->vertices[i].first_arc = NULL;
+    }
+    //4. 构造邻接矩阵
+    struct edge edges[] = {
+            {'A','B',6},{'A','C',1},{'A','D',5},
+            {'C','B',5},{'D','C',5},{'B','E',3},
+            {'C','E',6},{'F','C',4},{'F','D',2},
+            {'F','E',6}
+    };
+    for (int i = 0; i < G->arc_num; ++i) {
+        char a,b;
+        int w;
+        a = edges[i].a;
+        b = edges[i].b;
+        w = edges[i].weight;
+        int j = LocateVex(*G,a);
+        int k = LocateVex(*G,b);
+        ArcNode * newNode1 = (ArcNode*) malloc(sizeof (ArcNode));
+        ArcNode * newNode2 = (ArcNode*) malloc(sizeof (ArcNode));
+        newNode1->info = newNode2->info = (int *) malloc(sizeof (int ));
+        newNode1->adj_vex = k;*(newNode1->info) = w;newNode1->next_arc=NULL;
+        newNode2->adj_vex = j;*(newNode2->info) = w;newNode2->next_arc=NULL;
+        ArcNode *p;
+        if (G->vertices[j].first_arc == NULL || newNode1->adj_vex < G->vertices[j].first_arc->adj_vex) {
+            newNode1->next_arc = G->vertices[j].first_arc;
+            G->vertices[j].first_arc = newNode1;
+        } else {
+            p = G->vertices[j].first_arc;
+            while (p->next_arc != NULL && p->next_arc->adj_vex < k) p=p->next_arc;
+            newNode1->next_arc = p->next_arc;
+            p->next_arc = newNode1;
+        }
+
+        if (G->vertices[k].first_arc == NULL || newNode2->adj_vex < G->vertices[k].first_arc->adj_vex) {
+            newNode2->next_arc = G->vertices[k].first_arc;
+            G->vertices[k].first_arc = newNode2;
+        } else {
+            p = G->vertices[k].first_arc;
+            while (p->next_arc != NULL && p->next_arc->adj_vex < j) p=p->next_arc;
+            newNode2->next_arc = p->next_arc;
+            p->next_arc = newNode2;
+        }
 
 
     }
