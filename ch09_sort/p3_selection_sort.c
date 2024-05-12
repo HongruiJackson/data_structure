3fa1d7198276d1875dc7c1a63c640474fd640b1a
JacksonZHR <1024060689@qq.com>
2024-05-12 21:11:10 +0800

    feat: 堆排序

--- ch09_sort/p3_selection_sort.c
+++ ch09_sort/p3_selection_sort.c
@@ -15,6 +15,7 @@ typedef struct {
     int length; // 顺序表长度
 }SqList;
 
+void PrintInfo(SqList sqList, char name[]);
 /**
  * 初始化一个数组
  * @param sqList
@@ -45,6 +46,38 @@ void SimpleSelectionSort(SqList * sqList) {
     }
 }
 
+void HeapAdjust(SqList * sqList, int s, int m) {
+    // (s,m]索引的结点满足大顶堆，s不一定满足
+    int rc = sqList->r[s].key;
+    for (int i = 2*s+1; i < m; i = 2*i+1) { // i是左孩子，// 根据索引值的不同此处i和m的设置和变化会有些许不同
+        // 首先比较左右子树的根的大小，为了防止没有右子树的情况，i最大只能为m-2
+        if (i<m-1 && sqList->r[i].key < sqList->r[i+1].key) i++;
+        // 本就是大于等于的情况意味着不需要交换位置
+        if (rc >= sqList->r[i].key) break;
+        sqList->r[s] = sqList->r[i];
+        s = i;// 找下一个子树的情况
+    }
+    sqList->r[s].key = rc; // 最后空出来的位置
+}
+
+void HeapSort(SqList * sqList) {
+    for (int i = (sqList->length-2)/2; i >=0 ; --i) {
+        HeapAdjust(sqList,i,sqList->length);
+    }
+    PrintInfo(*sqList, "heap");
+    for (int i = sqList->length-1; i >0 ; --i) {
+        int temp = sqList->r[i].key;
+        sqList->r[i].key = sqList->r[0].key;
+        sqList->r[0].key = temp;
+        HeapAdjust(sqList,0,i);
+    }
+
+}
+
+
+
+
+
 
 void PrintInfo(SqList sqList, char name[]) {
     printf("%s: ",name);
@@ -59,4 +92,8 @@ int main() {
     InitExampleList(&sqList);
     SimpleSelectionSort(&sqList);
     PrintInfo(sqList, "simple selection sort");
+
+    InitExampleList(&sqList);
+    HeapSort(&sqList);
+    PrintInfo(sqList, "heap sort");
 }
